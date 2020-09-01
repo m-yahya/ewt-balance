fabc4c778e241bfd456d0361dbb2651bf058e8de
Muhammad Yahya <yahya_gis@hotmail.com>
2020-09-01 14:50:11 +0200

    Apply code formatting and update log output

--- src/getBlockNumber.js
+++ src/getBlockNumber.js
@@ -1,19 +1,21 @@
-const { web3, timeConverter, getBalance } = require('./utils')
-const walletAddress = '0x86a5A44CFf58638784c2028e7181CEDe57933321'
+const { web3, timeConverter, getBalance } = require("./utils");
+const walletAddress = "0x86a5A44CFf58638784c2028e7181CEDe57933321";
 
 async function main() {
-  const latestBlockNumber = await web3.eth.getBlockNumber()
+  const latestBlockNumber = await web3.eth.getBlockNumber();
 
-  console.time('totalTime')
-  for (let i = 7041225; i < latestBlockNumber; i++) {
-    const block = await web3.eth.getBlock(i)
-    const time = timeConverter(block.timestamp)
-    if (time === '2020-08-01 05:00:00') {
-      const balance = await getBalance(walletAddress, i)
-      console.log(i, time, balance)
+  console.time("totalTime");
+  for (let i = 7439848; i < latestBlockNumber; i++) {
+    const block = await web3.eth.getBlock(i);
+    const time = timeConverter(block.timestamp);
+    if (time === "2020-10-01 00:00:00") {
+      const balance = await getBalance(walletAddress, i);
+      console.log(
+        `Block No.: ${i}, Local Time: ${time}, Timestamp [UNIX]: ${block.timestamp}, Balance [wei]: ${balance}`
+      );
     }
   }
-  console.timeEnd('totalTime')
+  console.timeEnd("totalTime");
 }
 
-main()
+main();

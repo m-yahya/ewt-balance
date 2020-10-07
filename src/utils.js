58a58b770e6815bd91c65ea5c0ff077923048c8a
Muhammad Yahya <yahya_gis@hotmail.com>
2020-10-07 09:34:52 +0200

    Update node URL for webserver

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ const Web3 = require("web3");
 // set up web3 provider
 //const volta = 'ws://80.158.39.62:8546';
 //const volta = 'ws://80.158.47.134:7546';
-const web3 = new Web3("ws://80.158.20.81:8546");
+const web3 = new Web3("ws://80.158.47.134:8546");
 
 // covert UNIX timestamp to human readable format
 const timeConverter = (UNIX_timestamp) => {

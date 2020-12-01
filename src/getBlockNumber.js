d85d9750e893cac82316412b9f1b2ff495948a4d
m-yahya <yahya_gis@hotmail.com>
2020-12-01 09:10:10 +0100

    Update block number for January 2021

--- src/getBlockNumber.js
+++ src/getBlockNumber.js
@@ -5,10 +5,10 @@ async function main() {
   const latestBlockNumber = await web3.eth.getBlockNumber();
 
   console.time("totalTime");
-  for (let i = 9076181; i < latestBlockNumber; i++) {
+  for (let i = 961674; i < latestBlockNumber; i++) {
     const block = await web3.eth.getBlock(i);
     const time = timeConverter(block.timestamp);
-    if (time === "2020-12-01 00:00:00") {
+    if (time === "2021-01-01 00:00:00") {
       const balance = await getBalance(walletAddress, i);
       console.log(
         `Block No.: ${i}, Local Time: ${time}, Timestamp [UNIX]: ${block.timestamp}, Balance [wei]: ${balance}`

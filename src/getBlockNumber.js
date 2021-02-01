08599e18b2f01c800471c54f26cc1f501f17e9a6
m-yahya <yahya_gis@hotmail.com>
2021-02-01 07:56:55 +0100

    Calculate balance for Feb 2021

--- src/getBlockNumber.js
+++ src/getBlockNumber.js
@@ -5,10 +5,10 @@ async function main() {
   const latestBlockNumber = await web3.eth.getBlockNumber();
 
   console.time("totalTime");
-  for (let i = 9545000; i < latestBlockNumber; i++) {
+  for (let i = 10063341; i < latestBlockNumber; i++) {
     const block = await web3.eth.getBlock(i);
     const time = timeConverter(block.timestamp);
-    if (time === "2021-01-01 00:00:00") {
+    if (time === "2021-02-01 00:00:00") {
       const balance = await getBalance(walletAddress, i);
       console.log(
         `Block No.: ${i}, Local Time: ${time}, Timestamp [UNIX]: ${block.timestamp}, Balance [wei]: ${balance}`

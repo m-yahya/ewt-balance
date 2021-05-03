656e0c36cc38ffbee688449cd425bdea50a1a210
m-yahya <yahya_gis@hotmail.com>
2021-05-03 10:27:52 +0200

    Calculate balance for 30.04.21

--- src/getBlockNumber.js
+++ src/getBlockNumber.js
@@ -2,19 +2,18 @@ const { web3, timeConverter, getBalance } = require("./utils");
 const walletAddress = "0x86a5A44CFf58638784c2028e7181CEDe57933321";
 //const walletAddress = "0x54809eA74BECdD734d2C4329729835ab39BB23F3";
 
-
 async function main() {
   const latestBlockNumber = await web3.eth.getBlockNumber();
-  for (let i = 11589857; i < latestBlockNumber; i++) {
+  for (let i = 11572590; i < latestBlockNumber; i++) {
     const block = await web3.eth.getBlock(i);
     const time = timeConverter(block.timestamp);
-    if (time === "2021-05-01 00:00:00") {
-    const balance = await getBalance(walletAddress, i);
-    console.log(
-      `Block No.: ${i}, Local Time: ${time}, Timestamp [UNIX]: ${block.timestamp}, Balance [wei]: ${balance}`
-    );
+    if (time === "2021-04-30 00:00:00") {
+      const balance = await getBalance(walletAddress, i);
+      console.log(
+        `Block No.: ${i}, Local Time: ${time}, Timestamp [UNIX]: ${block.timestamp}, Balance [wei]: ${balance}`
+      );
+    }
   }
-   }
   console.timeEnd("totalTime");
 }
 

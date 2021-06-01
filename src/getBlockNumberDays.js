903e23d56e7bdfcc96c37c13c05ab63ea850b264
m-yahya <yahya_gis@hotmail.com>
2021-06-01 16:00:57 +0200

    Apply refactoring

--- src/getBlockNumberDays.js
+++ src/getBlockNumberDays.js
@@ -1,20 +1,23 @@
-const { web3, timeConverter, getBalance } = require("./utils");
-const walletAddress = "0x86a5A44CFf58638784c2028e7181CEDe57933321";
+const { web3, timeConverter } = require("./utils/helpers");
+const { BLOCKS } = require("./utils/constants");
+
+let array = [];
 
 async function main() {
+  let index = BLOCKS[0] + 17260;
   const latestBlockNumber = await web3.eth.getBlockNumber();
   console.time("totalTime");
-  for (let i = 12087407; i < latestBlockNumber; i++) {
-    const block = await web3.eth.getBlock(i);
+  for (index; index < latestBlockNumber; index++) {
+    const block = await web3.eth.getBlock(index);
     const time = timeConverter(block.timestamp);
     const timeslice = time.slice(-8);
     if (timeslice === "00:00:00") {
-      const balance = await getBalance(walletAddress, i);
-      console.log(
-        `Block No.: ${i}, Local Time: ${time}, Timestamp [UNIX]: ${block.timestamp}, Balance [wei]: ${balance}`
-      );
+      console.log(`Pushed: ${index}`);
+      array.unshift(index);
     }
   }
+
+  console.log(array);
   console.timeEnd("totalTime");
 }
 

d5f90ec7e438c0e81598862c449b3325ff55da13
m-yahya <yahya_gis@hotmail.com>
2021-06-01 12:30:44 +0200

    Update block number

--- src/getBlockNumberDays.js
+++ src/getBlockNumberDays.js
@@ -1,10 +1,10 @@
 const { web3, timeConverter, getBalance } = require("./utils");
-const walletAddress = "0x54809eA74BECdD734d2C4329729835ab39BB23F3";
+const walletAddress = "0x86a5A44CFf58638784c2028e7181CEDe57933321";
 
 async function main() {
   const latestBlockNumber = await web3.eth.getBlockNumber();
   console.time("totalTime");
-  for (let i = 11624374; i < latestBlockNumber; i++) {
+  for (let i = 12087407; i < latestBlockNumber; i++) {
     const block = await web3.eth.getBlock(i);
     const time = timeConverter(block.timestamp);
     const timeslice = time.slice(-8);
